c8bca1a789e3d77d1a20928573465c9af1b555ee
Timothy Moore <mtimothy984@gmail.com>
2020-07-09 08:57:04 -0700

    Retry cors failures automatically

--- src/js/glue/fetch_helper.js
+++ src/js/glue/fetch_helper.js
@@ -12,6 +12,27 @@ const api_fetch = (function() {
             url = apiBase + url;
         }
 
-        return fetch(url, kwargs);
+        return fetch(url, kwargs).catch((e) => {
+            if (e.message === 'Failed to fetch') {
+                if (!kwargs || !kwargs.headers || kwargs.headers['Cache-Control'] !== 'no-cache' || kwargs.headers['Pragma'] !== 'no-cache') {
+                    if (kwargs) {
+                        kwargs = {};
+                    } else {
+                        kwargs = Object.assign({}, kwargs);
+                    }
+
+                    if (kwargs.headers) {
+                        kwargs.headers = Object.assign({}, kwargs.headers);
+                    } else {
+                        kwargs.headers = {};
+                    }
+
+                    kwargs.headers['Cache-Control'] = 'no-cache';
+                    kwargs.headers['Pragma'] = 'no-cache';
+                    return api_fetch(url, kwargs);
+                }
+            }
+            return Promise.reject(e);
+        });
     };
 })();
