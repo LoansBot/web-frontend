aa528f72cf93cac61b00b72f85d2a302c06050e2
Timothy Moore <mtimothy984@gmail.com>
2020-08-04 08:02:53 -0700

    Fix currentPage not defined

--- src/js/demographics_by_user.js
+++ src/js/demographics_by_user.js
@@ -1,7 +1,7 @@
 (function() {
     ReactDOM.render(
         React.createElement(React.Fragment, null, [
-            React.createElement(NavbarLoader, {key: 'navbar', currentPage: currentPage}),
+            React.createElement(NavbarLoader, {key: 'navbar', currentPage: 'others-demographics'}),
             React.createElement('div', {key: 'main', className: 'main'}, [
                 React.createElement(
                     DemographicsByUserAjaxAndView,
