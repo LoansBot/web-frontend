f08d6b78bf66182c6c802453516839f9281c24f5
Timothy Moore <mtimothy984@gmail.com>
2020-11-01 09:03:26 -0800

    Fix undefined user id (#25)

--- src/js/components/trusts.js
+++ src/js/components/trusts.js
@@ -3342,7 +3342,7 @@ const [
                     React.createElement(
                         UserTrustControlsWithAjax,
                         {
-                            key: `controls-${this.props.userID}-${this.state.trustViewCounter}`,
+                            key: `controls-${this.props.userId}-${this.state.trustViewCounter}`,
                             targetUserId: this.props.userId,
                             queueItemUuid: this.props.queueItemUuid,
                             onChanged: this.onTrustStatusChanged
